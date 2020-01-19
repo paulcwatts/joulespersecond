af808caf6fb829c45b432db2395987a0fced2f92
Paul Watts <paulcwatts@gmail.com>
2020-01-19 12:21:54 -0800

    Initial content
    This is basically done, except for the animation I want to do.

---
+++ tests/integration/components/content-col-test.js
@@ -0,0 +1,23 @@
+import { module, test } from "qunit";
+import { setupRenderingTest } from "ember-qunit";
+import { render } from "@ember/test-helpers";
+import { hbs } from "ember-cli-htmlbars";
+
+module("Integration | Component | content-col", function(hooks) {
+  setupRenderingTest(hooks);
+
+  test("it renders", async function(assert) {
+    await render(hbs`
+      <ContentCol data-test-element @href="http://foo.com" />
+    `);
+    assert.dom("[data-test-element]").hasAttribute("href", "http://foo.com");
+
+    // Template block usage:
+    await render(hbs`
+      <ContentCol data-test-element @href="http://foo.com">
+        template block text
+      </ContentCol>
+    `);
+    assert.dom("[data-test-element").hasText("template block text");
+  });
+});
